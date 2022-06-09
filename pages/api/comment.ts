bc823896a9128551afe07aa888060d9d5730ea97
itrodi <54353764+itrodi@users.noreply.github.com>
2022-06-09 14:25:45 +0100

    Update comment.ts

--- pages/api/comment.ts
+++ pages/api/comment.ts
@@ -1,13 +1,1 @@
-import { GraphQLClient, gql } from 'graphql-request';
 
-const graphqlAPI = process.env.NEXT_PUBLIC_GRAPHCMS_ENDPOINT;
-
-/** *************************************************************
-* Any file inside the folder pages/api is mapped to /api/* and  *
-* will be treated as an API endpoint instead of a page.         *
-*************************************************************** */
-
-// export a default function for API route to work
-export default async function asyc handler(req, res) {
-  
-}
