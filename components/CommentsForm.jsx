abbdb3e0437aed7a62f8a05d7102fd5f20b20d6e
itrodi <54353764+itrodi@users.noreply.github.com>
2022-06-09 14:23:01 +0100

    Update CommentsForm.jsx

--- components/CommentsForm.jsx
+++ components/CommentsForm.jsx
@@ -1,37 +1,10 @@
 import React, { useState, useEffect } from 'react';
 
 const CommentsForm = ({ slug }) => {
-  
-
-  const handlePostSubmission = () => {
-   
-  };
-
-  const onInputChange = () => {
-
-  };
 
   return (
-    <div className="bg-white shadow-lg rounded-lg p-8 pb-12 mb-8">
-      <h3 className="text-xl mb-8 font-semibold border-b pb-4">Leave a Reply</h3>
-      <div className="grid grid-cols-1 gap-4 mb-4">
-        <textarea  onChange={onInputChange} className="p-4 outline-none w-full rounded-lg h-40 focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" name="comment" placeholder="Comment" />
-      </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4 mb-4">
-        <input type="text"  onChange={onInputChange} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Name" name="name" />
-        <input type="email"  onChange={onInputChange} className="py-2 px-4 outline-none w-full rounded-lg focus:ring-2 focus:ring-gray-200 bg-gray-100 text-gray-700" placeholder="Email" name="email" />
-      </div>
-      <div className="grid grid-cols-1 gap-4 mb-4">
-        <div>
-          <input  onChange={onInputChange} type="checkbox" id="storeData" name="storeData" value="true" />
-          <label className="text-gray-500 cursor-pointer" htmlFor="storeData"> Save my name, email in this browser for the next time I comment.</label>
-        </div>
-      </div>
-    
-      <div className="mt-8">
-        <button type="button" onClick={handlePostSubmission} className="transition duration-500 ease hover:bg-indigo-900 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">Post Comment</button>
-     
-      </div>
+    <div >
+        comment
     </div>
   );
 };
